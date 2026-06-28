a03163e77785fc6b1f53863face70afe103f8928
Daksh <dakshrajsinghrathore119@gmail.com>
2026-06-28 12:11:26 +0530

    Initial commit:added part 4 exercises

---
+++ Part_04/01_YourFirstAccount/YourFirstAccount.java
@@ -0,0 +1,7 @@
+public class YourFirstAccount {
+    public static void main(String[] args) {
+        Account arto = new Account("rahul",100.0);
+        arto.deposit(20.0);
+        System.out.println(arto);
+    }
+}
