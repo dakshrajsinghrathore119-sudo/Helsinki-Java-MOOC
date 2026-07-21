ff913e035b682c69913b1fcac91a35b9e39e1e63
Daksh <dakshrajsinghrathore119@gmail.com>
2026-07-21 20:11:29 +0530

    Completed Excercise 11

---
+++ Part_05/11_Song/Song.java
@@ -0,0 +1,33 @@
+public class Song {
+    private String artist;
+    private String name;
+    private int durationInSeconds;
+
+    public Song(String artist, String name, int durationInSeconds) {
+        this.artist = artist;
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+    }
+    
+    public boolean equals(Object compared){
+        if(this==compared){
+            return true;
+        }
+        if(!(compared instanceof Song)){
+            return false;
+        }
+        
+        Song song = (Song)compared;
+        if(this.artist.equals(song.artist) && this.name.equals(song.name) &&
+           this.durationInSeconds == song.durationInSeconds){
+            return true;
+    }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+    }
+}
